e173591e101d93d86ccf917b486e0f9c90ba0752
Verena Ena <verenaparaluxe@gmail.com>
2018-04-28 12:43:54 +0200

    Fix: add movement speed

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ var Enemy = function(x,y,speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
+    this.x = this.x + this.speed * dt;
     // which will ensure the game runs at the same speed for
     // all computers.
 };
